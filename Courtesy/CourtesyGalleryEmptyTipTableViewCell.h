b70ed5e390d9d6d974f307075231cdf01d663880
i_82 <i.82@qq.com>
2016-02-22 23:57:47 +0800

    添加重构文件

---
+++ Courtesy/CourtesyGalleryEmptyTipTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  CourtesyGalleryEmptyTipTableViewCell.h
+//  Courtesy
+//
+//  Created by Zheng on 2/22/16.
+//  Copyright © 2016 82Flex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CourtesyGalleryEmptyTipTableViewCell : UITableViewCell
+
+@property (nonatomic, copy) NSString *titleText;
+@property (nonatomic, strong) UIImage *iconImage;
+
+@end
---
+++ Courtesy/CourtesyMyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CourtesyMainTableViewController.h
+//  Courtesy
+//
+//  Created by Zheng on 2/21/16.
+//  Copyright © 2016 82Flex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CourtesyMyTableViewController : UITableViewController
+
+@end
