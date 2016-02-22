5ede83291989f6769b7cac0fe0567de1858f5a59
i_82 <i.82@qq.com>
2016-02-22 20:22:40 +0800

    重构关于页面

--- Courtesy/CourtesyAboutViewController.h
+++ Courtesy/CourtesyAboutViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CourtesyAboutViewController : UIViewController
+@interface CourtesyAboutViewController : UITableViewController
 
 @end
--- Courtesy/CourtesyAboutViewController.m
+++ Courtesy/CourtesyAboutViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "CourtesyAboutViewController.h"
+#import "GlobalDefine.h"
 
 @interface CourtesyAboutViewController () <UIGestureRecognizerDelegate>
 
 @property (weak, nonatomic) IBOutlet UILabel *detailLabel;
+@property (weak, nonatomic) IBOutlet UILabel *appLabel;
 
 @end
 
@@ -18,9 +20,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // 跳转到官方网站的手势
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(detailLabelClicked:)];
     tapGesture.delegate = self;
     [_detailLabel addGestureRecognizer:tapGesture];
+    
+    // 应用展示标签
+    _appLabel.text = [NSString stringWithFormat:@"%@ V%@", APP_NAME_CN, VERSION_STRING];
 }
 
 - (void)detailLabelClicked:(UITapGestureRecognizer *)sender {
